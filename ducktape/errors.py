2b056fd2d62953f360d69e004b6da7ef040ffc20
Ewen Cheslack-Postava <ewen@confluent.io>
2015-08-24 18:38:17 -0700

    Add DucktapeError as base error class for this package.

--- ducktape/errors.py
+++ ducktape/errors.py
@@ -12,5 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class TimeoutError(RuntimeError):
+class DucktapeError(RuntimeError):
+    pass
+
+class TimeoutError(DucktapeError):
     pass
