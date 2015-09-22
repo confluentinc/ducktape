7d444d4e7b545ebd2a02f413678896224c92740e
Geoff Anderson <geoff@confluent.io>
2015-09-22 13:41:00 -0700

    Update unit test

--- tests/command_line/check_main.py
+++ tests/command_line/check_main.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from ducktape.command_line.main import setup_results_directory
+from ducktape.command_line.main import update_latest_symlink
 
 import os
 import os.path
@@ -38,10 +39,12 @@ class CheckSetupResultsDirectory(object):
         """Check results and symlink from scratch"""
         assert not os.path.exists(self.results_dir)
         setup_results_directory(self.results_root, self.results_dir)
+        update_latest_symlink(self.results_root, self.results_dir)
         self.validate_directories()
 
     def check_symlink(self):
         """Check "latest" symlink behavior"""
+
         # if symlink already exists
         old_results = os.path.join(self.results_root, "OLD")
         os.mkdir(old_results)
@@ -49,10 +52,12 @@ class CheckSetupResultsDirectory(object):
         assert os.path.islink(self.latest_symlink) and os.path.exists(self.latest_symlink)
 
         setup_results_directory(self.results_root, self.results_dir)
+        update_latest_symlink(self.results_root, self.results_dir)
         self.validate_directories()
 
         # Try again if symlink exists and points to nothing
         os.rmdir(self.results_dir)
         assert os.path.islink(self.latest_symlink) and not os.path.exists(self.latest_symlink)
         setup_results_directory(self.results_root, self.results_dir)
+        update_latest_symlink(self.results_root, self.results_dir)
         self.validate_directories()
