924aadccfed9d2ede271c3d1728eae8d3edbf4a3
Geoff Anderson <geoff@confluent.io>
2016-07-21 18:06:11 -0700

    Enable parallel tests, add scheduler

--- ducktape/tests/runner.py
+++ ducktape/tests/runner.py
@@ -45,6 +45,56 @@ class Receiver(object):
         self.socket.send(self.serde.serialize(event))
 
 
+class TestScheduler(object):
+    def __init__(self, test_context_list, runner):
+        self.runner = runner
+        self.test_context_list = test_context_list
+        self._sort_test_context_list()
+
+    def __len__(self):
+        return len(self.test_context_list)
+
+    def _sort_test_context_list(self):
+        """Replace self.test_context_list with a sorted shallow copy
+
+        Sort from largest cluster users to smallest
+        """
+        # sort from largest cluster users to smallest
+        self.test_context_list = sorted(self.test_context_list,
+                                        key=lambda tc: self.runner.expected_num_nodes(tc),
+                                        reverse=True)
+
+    def __iter__(self):
+        """This class is iterable"""
+        return self
+
+    def peek(self):
+        """Locate and return the next object to be scheduled, without removing it internally.
+
+        :return test_context for the next test to be scheduled
+        :raise RuntimeError if the scheduler is empty
+        """
+        if len(self) == 0:
+            raise RuntimeError("No more tests available")
+
+        for tc in self.test_context_list:
+            if self.runner.expected_num_nodes(tc) <= self.runner.cluster.num_available_nodes():
+                return tc
+
+        return None
+
+    def next(self):
+        """Get the next test"""
+        tc = self.peek()
+        self.test_context_list.remove(tc)
+        return tc
+
+    def add(self, test_context):
+        """Enqueue another test"""
+        self.test_context_list.append(test_context)
+        self._sort_test_context_list()
+
+
 class TestRunner(object):
     """Runs tests serially."""
 
@@ -61,10 +111,11 @@ class TestRunner(object):
         self.receiver = Receiver(port)
 
         self.session_context = session_context
+        self.max_parallel = session_context.max_parallel
         self.results = TestResults(self.session_context)
 
         self.proc_list = []
-        self.staged_tests = tests
+        self.scheduler = TestScheduler(tests, self)
         self._test_context = {t.test_id: t for t in tests}
         self._test_cluster = {}  # Track subcluster assigned to a particular test_id
         self.active_tests = {}
@@ -77,14 +128,15 @@ class TestRunner(object):
     def run_all_tests(self):
         self.results.start_time = time.time()
         self._log(logging.INFO, "starting test run with session id %s..." % self.session_context.session_id)
-        self._log(logging.INFO, "running %d tests..." % len(self.staged_tests))
+        self._log(logging.INFO, "running %d tests..." % len(self.scheduler))
 
-        while len(self.staged_tests) > 0 or len(self.active_tests) > 0:
+        while len(self.scheduler) > 0 or len(self.active_tests) > 0:
 
-            if len(self.active_tests) == 0:
-                next_test_context = self.staged_tests.pop()
+            while len(self.active_tests) <= self.max_parallel and len(self.scheduler) > 0 and self.scheduler.peek() is not None:
+                next_test_context = self.scheduler.next()
                 self._preallocate_subcluster(next_test_context)
                 self._run_single_test(next_test_context)
+
             try:
                 event = self.receiver.recv()
                 self._handle(event)
@@ -99,8 +151,24 @@ class TestRunner(object):
 
         return self.results
 
+    def expected_num_nodes(self, test_context):
+        """Helper method for deciding how many nodes we expect the given test to use."""
+        expected = test_context.expected_num_nodes
+        if expected is None:
+            # If there is no information on cluster usage, allocate entire cluster
+            if self.session_context.max_parallel > 1:
+                self._log(logging.WARNING,
+                          "Test %s has no cluster use metadata, so this test will not run in parallel with any others."
+                          % test_context.test_id)
+            return len(self.cluster)
+        else:
+            return expected
+
     def _run_single_test(self, test_context):
         """Start a test runner client in a subprocess"""
+        # Test is considered "active" as soon as we start it up in a subprocess
+        self.active_tests[test_context.test_id] = True
+
         proc = multiprocessing.Process(
             target=run_client,
             args=[
@@ -122,17 +190,8 @@ class TestRunner(object):
         :param test_context
         :return None
         """
-
-        expected_num_nodes = test_context.expected_num_nodes
-        if expected_num_nodes is None:
-            # If there is no information on cluster usage, allocate entire cluster
-            if self.session_context.max_parallel > 1:
-                self._log(logging.WARNING,
-                          "Test %s has no cluster use metadata, so this test will not run in parallel with any others."
-                          % test_context.test_id)
-            expected_num_nodes = len(self.cluster)
-
-        self._test_cluster[test_context.test_id] = FiniteSubcluster(self.cluster.alloc(expected_num_nodes))
+        expected = self.expected_num_nodes(test_context)
+        self._test_cluster[test_context.test_id] = FiniteSubcluster(self.cluster.alloc(expected))
 
     def _handle(self, event):
         self._log(logging.DEBUG, str(event))
@@ -156,9 +215,6 @@ class TestRunner(object):
         self.receiver.send(
                 self.event_response.ready(event, self.session_context, test_context, subcluster))
 
-        # Test is considered "active" as soon as we receive the ready request
-        self.active_tests[event["test_id"]] = event
-
     def _handle_log(self, event):
         self.receiver.send(self.event_response.log(event))
         self._log(event["log_level"], event["message"])
@@ -177,9 +233,9 @@ class TestRunner(object):
         self.cluster.free(subcluster.alloc(len(subcluster)))
         del self._test_cluster[test_id]
 
-        if len(self.staged_tests) + len(self.active_tests) > 0 and self.session_context.max_parallel == 1:
+        if len(self.scheduler) + len(self.active_tests) > 0 and self.session_context.max_parallel == 1:
             terminal_width, y = get_terminal_size()
-            print "~" * int(2 * terminal_width / 3)
+            self._log(logging.INFO, "~" * int(2 * terminal_width / 3))
 
     def _handle_lifecycle(self, event):
         self.receiver.send(self.event_response._event_response(event))
