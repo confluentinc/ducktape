474550edfb3c238056265c3daa7cff200e361238
Geoff Anderson <geoff@confluent.io>
2016-07-23 22:12:03 -0700

    Add (process)signal handling to main process

--- ducktape/tests/runner.py
+++ ducktape/tests/runner.py
@@ -15,6 +15,8 @@
 
 import logging
 import multiprocessing
+import os
+import signal
 import time
 import traceback
 import zmq
@@ -53,6 +55,10 @@ class TestRunner(object):
     stop_testing = False
 
     def __init__(self, cluster, session_context, session_logger, tests, port=ConsoleDefaults.TEST_DRIVER_PORT):
+        # Set handlers for SIGTERM and SIGINT (kill -15, and Ctrl-C, respectively)
+        signal.signal(signal.SIGTERM, self._propagate_sigterm)
+        signal.signal(signal.SIGINT, self._propagate_sigterm)
+
         # session_logger, message logger,
         self.session_logger = session_logger
         self.cluster = cluster
@@ -65,41 +71,81 @@ class TestRunner(object):
         self.max_parallel = session_context.max_parallel
         self.results = TestResults(self.session_context)
 
-        self.proc_list = []
+        self.main_process_pid = os.getpid()
         self.scheduler = TestScheduler(
             [TestExpectedNodes(test_context=t, expected_nodes=self.expected_num_nodes(t)) for t in tests],
             self.cluster)
         self._test_context = {t.test_id: t for t in tests}
         self._test_cluster = {}  # Track subcluster assigned to a particular test_id
+        self._client_procs = {}  # track client processes running tests
         self.active_tests = {}
         self.finished_tests = {}
+
+    def _propagate_sigterm(self, signum, frame):
+        """Handler SIGTERM and SIGINT by propagating SIGTERM to all client processes.
+
+        Note that multiprocessing processes are in the same process group as the main process, so Ctrl-C will
+        result in SIGINT being propagated to all client processes automatically. This may result in multiple SIGTERM
+        signals getting sent to client processes in quick succession.
+
+        However, it is possible that the main process (and not the process group) receives a SIGINT or SIGTERM
+        directly. Propagating SIGTERM to client processes is necessary in this case.
+        """
+
+        if os.getpid() != self.main_process_pid:
+            # since we're using the multiprocessing module to create client processes,
+            # this signal handler is also attached client processes, so we only want to propagate TERM signals
+            # if this process *is* the main runner server process
+            return
+
+        self.stop_testing = True
+        for p in self._client_procs.values():
+
+            # this handler should be a noop if we're in a client process, so it's an error if the current pid
+            # is in self._client_procs
+            assert p.pid != os.getpid(), "Signal handler should not reach this point in a client subprocess."
+            if p.is_alive():
+                os.kill(p.pid, signal.SIGTERM)
 
     def who_am_i(self):
         """Human-readable name helpful for logging."""
         return self.__class__.__name__
+
+    @property
+    def _ready_to_trigger_more_tests(self):
+        """Should we pull another test from the scheduler?"""
+        return not self.stop_testing and \
+            len(self.active_tests) <= self.max_parallel and \
+            len(self.scheduler) > 0 and \
+            self.scheduler.peek() is not None
+
+    @property
+    def _expect_client_requests(self):
+        return len(self.active_tests) > 0
 
     def run_all_tests(self):
         self.results.start_time = time.time()
         self._log(logging.INFO, "starting test run with session id %s..." % self.session_context.session_id)
         self._log(logging.INFO, "running %d tests..." % len(self.scheduler))
 
-        while len(self.scheduler) > 0 or len(self.active_tests) > 0:
+        while self._ready_to_trigger_more_tests or self._expect_client_requests:
 
-            while len(self.active_tests) <= self.max_parallel and len(self.scheduler) > 0 and self.scheduler.peek() is not None:
+            while self._ready_to_trigger_more_tests:
                 next_test_context = self.scheduler.next()
                 self._preallocate_subcluster(next_test_context)
                 self._run_single_test(next_test_context)
 
-            try:
-                event = self.receiver.recv()
-                self._handle(event)
-            except Exception as e:
-                err_str = "Exception receiving message: %s: %s" % (str(type(e)), str(e))
-                err_str += "\n" + traceback.format_exc(limit=16)
-                self._log(logging.ERROR, err_str)
-                continue
+            if self._expect_client_requests:
+                try:
+                    event = self.receiver.recv()
+                    self._handle(event)
+                except Exception as e:
+                    err_str = "Exception receiving message: %s: %s" % (str(type(e)), str(e))
+                    err_str += "\n" + traceback.format_exc(limit=16)
+                    self._log(logging.ERROR, err_str)
+                    continue
 
-        for proc in self.proc_list:
+        for proc in self._client_procs.values():
             proc.join()
 
         return self.results
@@ -124,7 +170,8 @@ class TestRunner(object):
                 test_context.results_dir,
                 self.session_context.debug
             ])
-        self.proc_list.append(proc)
+
+        self._client_procs[test_context.test_id] = proc
         proc.start()
 
     def _preallocate_subcluster(self, test_context):
@@ -172,10 +219,10 @@ class TestRunner(object):
         self._log(event["log_level"], event["message"])
 
     def _handle_finished(self, event):
+        test_id = event["test_id"]
         self.receiver.send(self.event_response.finished(event))
 
         # Transition this test from running to finished
-        test_id = event["test_id"]
         del self.active_tests[test_id]
         self.finished_tests[test_id] = event
         self.results.append(event['result'])
@@ -185,9 +232,18 @@ class TestRunner(object):
         self.cluster.free(subcluster.alloc(len(subcluster)))
         del self._test_cluster[test_id]
 
-        if len(self.scheduler) + len(self.active_tests) > 0 and self.session_context.max_parallel == 1:
+        # Join on the finished test process
+        self._client_procs[test_id].join()
+
+        if self._should_print_delimiter:
             terminal_width, y = get_terminal_size()
             self._log(logging.INFO, "~" * int(2 * terminal_width / 3))
+
+    @property
+    def _should_print_delimiter(self):
+        return self.session_context.max_parallel == 1 and \
+            not self.stop_testing and \
+            (self._expect_client_requests or self._ready_to_trigger_more_tests)
 
     def _handle_lifecycle(self, event):
         self.receiver.send(self.event_response._event_response(event))
