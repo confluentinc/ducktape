0c5f4be285e399f471376902e9a6fd662a1a63b7
Geoff Anderson <geoff@confluent.io>
2016-08-31 10:51:33 -0700

    Fix "No handlers available..." message

--- ducktape/tests/runner_client.py
+++ ducktape/tests/runner_client.py
@@ -152,13 +152,14 @@ class RunnerClient(object):
 
             result.report()
 
-            # Release test_context resources only after creating the result and finishing logging activity
-            self.test_context.close()
-            self.test_context = None
-            self.test = None
-
         # Tell the server we are finished
         self._do_safely(lambda: self.send(self.message.finished(result=result)), "Problem sending FINISHED message:")
+
+        # Release test_context resources only after creating the result and finishing logging activity
+        # The Sender object uses the same logger, so we postpone closing until after the finished message is sent
+        self.test_context.close()
+        self.test_context = None
+        self.test = None
 
     def setup_test(self):
         """start services etc"""
