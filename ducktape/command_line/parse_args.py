3337c4abda37c89d8c4e4bfe5d48b08e979f5cbb
John Spong <jspong@gmail.com>
2020-03-25 15:15:21 -0400

    add ability to run --sample of tests

--- ducktape/command_line/parse_args.py
+++ ducktape/command_line/parse_args.py
@@ -70,6 +70,8 @@ def create_ducktape_parser():
                         help="URL of a JSON report file containing stats from a previous test run. If specified, "
                              "this will be used when creating subsets of tests to divide evenly by total run time "
                              "instead of by number of tests.")
+    parser.add_argument("--sample", action="store", type="int",
+                        help="The size of a random test sample to run")
     return parser
 
 
