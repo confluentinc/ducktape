419b43dc864ddda3c72158c54fd218e6ef082e5f
Jerry Wang <j3rry.wan9@gmail.com>
2021-02-07 23:46:44 -0800

    Add a unit test

--- tests/command_line/check_main.py
+++ tests/command_line/check_main.py
@@ -155,3 +155,20 @@ class CheckUserDefinedGlobals(object):
 
         with pytest.raises(ValueError):
             get_user_defined_globals(valid_json_not_dict)
+
+
+    def check_non_dict_from_file(self):
+        """Validate behavior when given file containing valid JSON which does not parse as a dict"""
+        _, fname = tempfile.mkstemp()
+        try:
+            with open(fname, "w") as fh:
+                # Write valid JSON which does not parse as a dict
+                fh.write(valid_json_not_dict)
+
+            with pytest.raises(ValueError) as excinfo:
+                get_user_defined_globals(fname)
+
+            assert fname in str(excinfo.value)
+
+        finally:
+            os.remove(fname)
