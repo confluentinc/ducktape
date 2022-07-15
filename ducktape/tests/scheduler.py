e7cf0f6ebd68a6574112c78f9e395ead28d6a811
Stanislav Vodetskyi <stan@confluent.io>
2022-07-14 17:50:44 -0700

    dont use copy

--- ducktape/tests/scheduler.py
+++ ducktape/tests/scheduler.py
@@ -25,7 +25,10 @@ class TestScheduler(object):
 
         # Track tests which would never be offered up by the scheduling algorithm due to insufficient
         # cluster resources
-        self._test_context_list = test_contexts.copy()
+        # self._test_context_list = test_contexts.copy()
+        self._test_context_list = []
+        for tc in test_contexts:
+            self._test_context_list.append(tc)
 
         self._sort_test_context_list()
 
