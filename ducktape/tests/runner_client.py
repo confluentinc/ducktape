f6797e3a2e6153a617c9c65bf3bab014176f3668
imcdo <imcdonald@confluent.io>
2022-10-13 15:21:38 -0700

    cleanup imports

--- ducktape/tests/runner_client.py
+++ ducktape/tests/runner_client.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections import Counter, defaultdict
+from collections import defaultdict
 import logging
 import os
 import signal
