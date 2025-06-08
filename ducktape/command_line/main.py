e7520089268d91cc231533f1382ca3b16e44aff3
Tarun Srivatsa V S <tsrivatsavs@confluent.io>
2025-06-08 23:49:15 +0530

    revert "temp: introduce fault in expected_test_count for testing"

--- ducktape/command_line/main.py
+++ ducktape/command_line/main.py
@@ -200,8 +200,6 @@ def main():
     runner = TestRunner(cluster, session_context, session_logger, tests, deflake_num)
     test_results = runner.run_all_tests()
 
-    expected_test_count += 1  # TODO: this is for testing purposes, remove later
-
     # Report results
     reporters = [
         SimpleStdoutSummaryReporter(test_results),
