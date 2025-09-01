74016aa45592c93e875a7b2ac1b13da6ba02d2cf
Saurabh Yadav <syadav@confluent.io>
2025-09-01 12:01:00 +0530

    Stop sending logs to server when shutting down due to SIGTERM signal from server as server is no longer listening

--- ducktape/tests/runner_client.py
+++ ducktape/tests/runner_client.py
@@ -164,6 +164,7 @@ class RunnerClient(object):
         self.test = None
         self.test_context = None
         self.all_services = None
+        self.shutting_down = False
 
     @property
     def deflake_enabled(self) -> bool:
@@ -191,6 +192,7 @@ class RunnerClient(object):
 
         python will treat SIGINT as a Keyboard exception. Exception handling does the rest.
         """
+        self.shutting_down = True
         self.logger.warning("Received SIGTERM, sending SIGINT to self and all child processes")
         self._kill_all_child_processes(signal.SIGINT)
         os.kill(os.getpid(), signal.SIGINT)  # This will send SIGINT to the current process
@@ -478,8 +480,8 @@ class RunnerClient(object):
                 str(msg),
             )
             self.logger.log(log_level, msg, *args, **kwargs)
-
-        self.send(self.message.log(msg, level=log_level))
+        if not self.shutting_down:
+            self.send(self.message.log(msg, level=log_level))
 
     def dump_threads(self, msg):
         dump = "\n".join([t.name for t in threading.enumerate()])
